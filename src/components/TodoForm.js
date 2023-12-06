8e4dbc907c82369a8e26300627a63b40630e1b47
mtsfreitas <mtsftsmts@gmail.com>
2023-12-06 17:08:43 -0300

    task manager completed

---
+++ src/components/TodoForm.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+export const TodoForm = ({addTodo}) => {
+    const [value, setValue] = useState("")
+
+    const handleSubmit = e => {
+      e.preventDefault();
+
+      addTodo(value);
+
+    }
+
+  return (
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type="text" className='todo-input' placeholder='Write your task here' onChange={(e) => setValue(e.target.value)}/>
+        <button type='submit' className='todo-btn'>Add Task</button>
+    </form>
+  )
+}
